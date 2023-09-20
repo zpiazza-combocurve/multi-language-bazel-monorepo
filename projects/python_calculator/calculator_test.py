84e5cdfc189fe7d67285fc769701bccc187ed3e8
Zach Piazza <z.piazza@combocurve.com>
2023-09-20 17:41:53 -0500

    fix(calculator): add additional test case

--- projects/python_calculator/calculator_test.py
+++ projects/python_calculator/calculator_test.py
@@ -9,6 +9,10 @@ class TestSum(unittest.TestCase):
   def test_sum_negative(self):
     calculator = Calculator()
     self.assertEqual(calculator.add(-4, -11), -15)
+  
+  def test_sum_zero(self):
+    calculator = Calculator()
+    self.assertEqual(calculator.add(0, 0), 0)
 
 if __name__ == '__main__':
   unittest.main()
